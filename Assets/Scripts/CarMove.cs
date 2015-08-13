501a636d2ea573d1a59aa06c0d2815980a199734
노효빈 <gyqls0309@naver.com>
2015-08-13 10:59:10 +0900

    no message

---
+++ Assets/Scripts/CarMove.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarMove : MonoBehaviour {
+
+	public bool engine = true;
+
+	public float speed = 10;
+	
+	void Start () {
+	
+	}
+
+	void Update () {
+	
+	}
+
+	public void RotateRight(){
+
+	}
+
+	public void RotateLeft(){
+
+	}
+
+	public void RotateUp(){
+
+	}
+
+	public void RotateDown(){
+
+	}
+
+}
