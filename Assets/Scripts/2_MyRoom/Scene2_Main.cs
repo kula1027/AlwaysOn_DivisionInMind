6095b2adc1d3b0139e3f436fa5edb58f8c7dbc69
노효빈 <gyqls0309@naver.com>
2015-08-13 13:22:57 +0900

    #2 MyRoom 대강 꾸며놓음 다음 씬으로 넘어갈 수 있음. 기본 틀 잡아놓음

---
+++ Assets/Scripts/2_MyRoom/Scene2_Main.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scene2_Main : MonoBehaviour {
+
+	public int targetShipNumber = 0;
+	public GameObject cam;
+
+	void Start () {
+	
+	}
+
+	void Update () {
+		switch(targetShipNumber){
+		case 0:
+			cam.transform.rotation = Quaternion.Lerp(cam.transform.rotation, Quaternion.Euler(new Vector3(5.8f, -56.43f, 0)), 0.1f);
+			break;
+		case 1:
+			cam.transform.rotation = Quaternion.Lerp(cam.transform.rotation, Quaternion.Euler(new Vector3(5.8f, 0, 0)), 0.1f);
+			break;
+		case 2:
+			cam.transform.rotation = Quaternion.Lerp(cam.transform.rotation, Quaternion.Euler(new Vector3(5.8f, 44.44f, 0)), 0.1f);
+			break;
+		}
+	}
+
+	public void OnButtonShift(bool right){
+		if(right){
+			if(targetShipNumber<2){
+				targetShipNumber ++;
+			}
+		}else{
+			if(0<targetShipNumber){
+				targetShipNumber--;
+			}
+		}
+	}
+
+	public void OnButtonStart(){
+		Application.LoadLevel("3_PlayerSelect");
+	}
+}
---
+++ Assets/Scripts/2_MyRoom/Scene2_ShipMoving.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scene2_ShipMoving : MonoBehaviour {
+
+	private float timer = 0;
+	private Vector3 firstPos;
+
+	void Start(){
+		firstPos = transform.position;
+	}
+
+	void Update () {
+		timer += Time.deltaTime;
+		if(10000000< timer){
+			timer = 0;
+		}
+		transform.position = new Vector3(0, Mathf.Sin(timer)*1, 0) + firstPos;
+		//transform.Rotate(new Vector3(0, Time.deltaTime * 5, 0));
+	}
+}
