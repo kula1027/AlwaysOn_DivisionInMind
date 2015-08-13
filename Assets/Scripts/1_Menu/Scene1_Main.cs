07e4bb58d25d1a7b1a696bed0c430306127abb5d
노효빈 <gyqls0309@naver.com>
2015-08-13 11:31:22 +0900

    #1 일단 아무렇게나 시작메뉴를 만듬. 다음 씬으로 넘어가도록 만들었음

---
+++ Assets/Scripts/1_Menu/Scene1_Main.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scene1_Main : MonoBehaviour {
+	
+	void Start () {
+	
+	}
+
+	void Update () {
+	
+	}
+
+	public void OnButtonStart(){
+		Application.LoadLevel("2_MyRoom");
+	}
+
+	public void OnButtonExit(){
+		Application.Quit();
+	}
+}
